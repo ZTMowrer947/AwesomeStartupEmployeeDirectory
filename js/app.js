2437c9f52be6ba499cadcff8ae882bbd29605448
ZTMowrer947 <Zackmowrer@gmail.com>
2018-12-21 18:10:14 -0500

    Add base employee modal functionality

--- js/app.js
+++ js/app.js
@@ -63,33 +63,60 @@ const onPageLoad = () => {
                 </div>
             `;
 
-            // Create the element and append to gallery
-            $(employeeHtml)
+            // Create card for employee
+            const $employeeCard = $(employeeHtml);
+
+            // Append employee card to gallery
+            $employeeCard
                 .appendTo($("#gallery"));
 
             // Event listener for click event on an employee item
-                /* Create modal for employee in the following format:
-                    <div class="modal-container">
-                        <div class="modal">
-                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                            <div class="modal-info-container">
-                                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                                <h3 id="name" class="modal-name cap">name</h3>
-                                <p class="modal-text">email</p>
-                                <p class="modal-text cap">city</p>
-                                <hr>
-                                <p class="modal-text">(555) 555-5555</p>
-                                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                                <p class="modal-text">Birthday: 10/21/2015</p>
+            $employeeCard
+                .on("click", () => {
+                    // Get employee date of birth
+                    const dob = new Date(employee.dob.date);
+
+                    // Set formatting options
+                    const formatOptions = {
+                        // 2-digit day (00 to 31)
+                        day: "2-digit",
+                        // 2-digit month (00 to 12)
+                        month: "2-digit",
+                        // Numeric year (2018)
+                        year: "numeric",
+                    }
+
+                    // Format date of birth into expected format
+                    const dobString = dob.toLocaleDateString(undefined, formatOptions);
+
+                    // HTML Markup for employee modal
+                    const modalHtml = `
+                        <div class="modal-container">
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                                    <p class="modal-text">${employee.email}</p>
+                                    <p class="modal-text cap">${employee.location.city}</p>
+                                    <hr>
+                                    <p class="modal-text">${employee.phone}</p>
+                                    <p class="modal-text">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                                    <p class="modal-text">Birthday: ${dobString}</p>
+                                </div>
                             </div>
                         </div>
+                    `;
 
-                        <div class="modal-btn-container">
-                            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                        </div>
-                    </div>
-                */
+                    // Create modal and append to body
+                    const $modal = $(modalHtml)
+                        .appendTo($("body"));
+
+                    // Attach listener to close button
+                    $("#modal-close-btn")
+                        // Close modal on click
+                        .on("click", () => $modal.remove());
+                });
 
             /* Insert search functionality with following format:
                 <form action="#" method="get">
