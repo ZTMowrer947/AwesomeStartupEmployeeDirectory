8885fd165133b9ef6b448aa22f704eb2ebe43ac0
ZTMowrer947 <Zackmowrer@gmail.com>
2018-12-26 23:05:13 -0500

    Modify modal formatting to have employee name as its own variable

--- js/app.js
+++ js/app.js
@@ -49,6 +49,9 @@ const createModalForEmployee = (employees, index) => {
     // Get employee
     const employee = employees[index];
 
+    // Employee full name
+    const employeeName = `${employee.name.first} ${employee.name.last}`;
+
     // Get employee date of birth
     const dob = new Date(employee.dob.date);
 
@@ -81,7 +84,7 @@ const createModalForEmployee = (employees, index) => {
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
                     <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <h3 id="name" class="modal-name cap">${employeeName}</h3>
                     <p class="modal-text">${employee.email}</p>
                     <p class="modal-text cap">${employee.location.city}</p>
                     <hr>
@@ -117,7 +120,7 @@ const createModalForEmployee = (employees, index) => {
     let nextIndex = index + 1;
 
     // Get prev and next buttons
-    const $prevButton = $("#modal-prev")
+    const $prevButton = $("#modal-prev");
     const $nextButton = $("#modal-next");
 
     // If the index of the next employee is out of bounds,
